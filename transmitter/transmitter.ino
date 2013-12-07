42bcac954d060cd0b54a760495f861132e19a7f9
Adam Pritchard <pritchard.adam@gmail.com>
2013-12-07 06:58:20 -0500

    Added support for magnetic reed switch, aka door closed sensor

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -5,11 +5,16 @@ uint8_t OPEN_MSG_CHAR = '1';
 
 int ledPin = 13;
 int txPin = 12;
+int doorPin = 8;
+
 
 void setup()
 {
   // We'll show the door state on the LED
   pinMode(ledPin, OUTPUT);
+  
+  pinMode(doorPin, INPUT);
+  digitalWrite(doorPin, HIGH);  // turn on pullup resistors
   
   vw_set_ptt_inverted(true);  // Required for RF Link Module
   vw_setup(2000);  // Bits per sec
@@ -17,23 +22,17 @@ void setup()
 }
 
 
-const unsigned long sendDuration = 2000;
-unsigned long flipTime = millis();  // TODO: Handle overflow
-uint8_t sendVal = OPEN_MSG_CHAR;
-uint8_t ledOut = HIGH;
-
 void loop()
 {
-  if ((millis() - flipTime) > sendDuration) {
-    flipTime = millis();
-    sendVal = (sendVal == OPEN_MSG_CHAR) ? CLOSED_MSG_CHAR : OPEN_MSG_CHAR;
-    ledOut = (sendVal == OPEN_MSG_CHAR) ? HIGH : LOW;
-  }
+  // When the switch is closed (i.e., the door is closed, the pin will read LOW
+  int doorState = digitalRead(doorPin);
+
+  uint8_t sendVal = (doorState == LOW) ? CLOSED_MSG_CHAR : OPEN_MSG_CHAR;
   
   vw_send(&sendVal, 1);
   vw_wait_tx();  // Wait for message to finish
 
-  digitalWrite(ledPin, ledOut);
+  digitalWrite(ledPin, doorState);
   delay(100);
 }
 
