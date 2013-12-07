663fced4e7028de913e6adfb0a4b37bcdf3af75e
Adam Pritchard <pritchard.adam@gmail.com>
2013-12-07 07:45:18 -0500

    Added light sensor. We're functionally complete now.

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -6,10 +6,13 @@ uint8_t OPEN_MSG_CHAR = '1';
 int ledPin = 13;
 int txPin = 12;
 int doorPin = 8;
+int photocellPin = A0;
 
 
 void setup()
 {
+  Serial.begin(9600);
+  
   // We'll show the door state on the LED
   pinMode(ledPin, OUTPUT);
   
@@ -22,17 +25,34 @@ void setup()
 }
 
 
+// When the switch is closed (i.e., the door is closed), the pin will read LOW
+const int DOOR_OPEN = HIGH;
+const int DOOR_CLOSED = LOW;
+
 void loop()
 {
-  // When the switch is closed (i.e., the door is closed, the pin will read LOW
   int doorState = digitalRead(doorPin);
+  
+  int photocellReading = analogRead(photocellPin);
+  
+  // We're going to leave in this serial debugging output because we might need to 
+  // adjust our light threshold for the particular installation in the particular room.
+  Serial.println(photocellReading);
+  
+  // Testing has shown that a photocell reading (with a 1K resistor) below
+  // about 100 is a good indicator of the bathroom light being off.
+  photocellReading = (photocellReading < 100) ? LOW : HIGH;
 
-  uint8_t sendVal = (doorState == LOW) ? CLOSED_MSG_CHAR : OPEN_MSG_CHAR;
+  // We consider the bathroom occupied if and only if the door is closed and the light is on.
+  uint8_t sendVal = OPEN_MSG_CHAR;
+  if (doorState == DOOR_CLOSED && photocellReading == HIGH) {
+    sendVal = CLOSED_MSG_CHAR;
+  }
   
   vw_send(&sendVal, 1);
   vw_wait_tx();  // Wait for message to finish
 
-  digitalWrite(ledPin, doorState);
+  digitalWrite(ledPin, sendVal == OPEN_MSG_CHAR);
   delay(100);
 }
 
